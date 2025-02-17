914baa3ec16922fcd7365a97b40f5612abbf0225
minhyeokshin <sinminhyeok@gmail.com>
2025-02-17 23:32:08 +0900

    ssg

--- src/exam2/DTO/Board.java
+++ src/exam2/DTO/Board.java
@@ -1,7 +1,5 @@
 package exam2.DTO;
 
-
-
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -16,45 +14,5 @@ public class Board {
     private String bdate;
 
     public static List<Board> boards = new ArrayList<>();
-
-    public int getBno() {
-        return bno;
-    }
-
-    public void setBno(int bno) {
-        this.bno = bno;
-    }
-
-    public String getBtitle() {
-        return btitle;
-    }
-
-    public void setBtitle(String btitle) {
-        this.btitle = btitle;
-    }
-
-    public String getBcontent() {
-        return bcontent;
-    }
-
-    public void setBcontent(String bcontent) {
-        this.bcontent = bcontent;
-    }
-
-    public String getBwriter() {
-        return bwriter;
-    }
-
-    public void setBwriter(String bwriter) {
-        this.bwriter = bwriter;
-    }
-
-    public String getBdate() {
-        return bdate;
-    }
-
-    public void setBdate(String bdate) {
-        this.bdate = bdate;
-    }
 
 }
--- src/exam2/Test.java
+++ src/exam2/Test.java
@@ -1,31 +1,31 @@
-package exam2;
-
-import exam2.Controller.Output;
-import exam2.DTO.Board;
-
-public class Test {
-    public static void main(String[] args) {
-
-        Board.boards.add(new Board());
-        Board.boards.add(new Board());
-        Board.boards.add(new Board());
-        Board.boards.get(0).setBno(1);
-        Board.boards.get(0).setBtitle("1");
-        Board.boards.get(0).setBcontent("1");
-        Board.boards.get(0).setBwriter("1");
-        Board.boards.get(0).setBdate("1");
-        Board.boards.get(1).setBno(2);
-        Board.boards.get(1).setBtitle("2");
-        Board.boards.get(1).setBcontent("2");
-        Board.boards.get(1).setBwriter("2");
-        Board.boards.get(1).setBdate("2");
-        Board.boards.get(2).setBno(3);
-        Board.boards.get(2).setBtitle("3");
-        Board.boards.get(2).setBcontent("3");
-        Board.boards.get(2).setBwriter("3");
-        Board.boards.get(2).setBdate("3");
-
-        Output.list();
-
-    }
-}
+//package exam2;
+//
+//import exam2.Controller.Output;
+//import exam2.DTO.Board;
+//
+//public class Test {
+//    public static void main(String[] args) {
+//
+//        Board.boards.add(new Board());
+//        Board.boards.add(new Board());
+//        Board.boards.add(new Board());
+//        Board.boards.get(0).setBno(1);
+//        Board.boards.get(0).setBtitle("1");
+//        Board.boards.get(0).setBcontent("1");
+//        Board.boards.get(0).setBwriter("1");
+//        Board.boards.get(0).setBdate("1");
+//        Board.boards.get(1).setBno(2);
+//        Board.boards.get(1).setBtitle("2");
+//        Board.boards.get(1).setBcontent("2");
+//        Board.boards.get(1).setBwriter("2");
+//        Board.boards.get(1).setBdate("2");
+//        Board.boards.get(2).setBno(3);
+//        Board.boards.get(2).setBtitle("3");
+//        Board.boards.get(2).setBcontent("3");
+//        Board.boards.get(2).setBwriter("3");
+//        Board.boards.get(2).setBdate("3");
+//
+//        Output.list();
+//
+//    }
+//}
