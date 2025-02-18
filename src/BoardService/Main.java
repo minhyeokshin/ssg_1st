8bd3c3cb708beaf5862da5c48ffde89f5b11ac71
minhyeokshin <sinminhyeok@gmail.com>
2025-02-18 16:39:54 +0900

    ssg

--- src/BoardService/Main.java
+++ src/BoardService/Main.java
@@ -4,7 +4,6 @@ import BoardService.Controller.BoardController;
 
 public class Main {
 
-
     public static void main(String[] args) {
        BoardController board = new BoardController();
         board.list();
